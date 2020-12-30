fcf5e81f7a2e0bfc57e5ee26c2c7b79a69179b87
tomryb <1rybak.tomasz@gmail.com>
2020-12-30 12:52:28 +0100

    issue 1 add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import InlineBackgroundColor from './InlineBackgroundColor';
+import InlineBackgroundColor from './components/zadanie1/InlineBackgroundColor';
 import CssExampleElement from './components/zadanie2/CssExampleElement';
 import ScssExampleElement from './components/zadanie3/ScssExampleElement';
 import BEMExampleHeader from './components/zadanie4/BEMExampleHeader';
@@ -9,16 +9,22 @@ const ContainerStyle = {
   backgroundColor: "blue"
 }
 
+const HeaderSize = {
+  fontSize: '20px'
+}
+
 function App() {
   const [color, setColor] = useState(false)
   const handleClick = () => setColor(!color)
 
+  const headerStyle = {
+    ...HeaderSize,
+    backgroundColor: color ? 'green' : 'yellow',
+  }
+
   return (
     <div style={ContainerStyle}>test
-      <h1 style={{
-        fontSize: '20px',
-        backgroundColor: color ? 'green' : 'yellow',
-      }}>Nagłówek</h1>
+      <h1 style={headerStyle}>Nagłówek</h1>
       <button onClick={handleClick}>Zmień kolor</button>
       <InlineBackgroundColor></InlineBackgroundColor>
       <CssExampleElement></CssExampleElement>
